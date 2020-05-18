4ca31c44284f5700e40ccac9356964a4eb3492a1
iliujiannan <1239068259@qq.com>
2020-05-18 16:00:09 +0800

    音调view

---
+++ app/src/main/java/com/example/guys/MarqueeTextView.kt
@@ -0,0 +1,18 @@
+package com.example.guys
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+/**
+ * Created by liujiannan on 2020-04-04
+ */
+class MarqueeTextView : TextView {
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {}
+
+    override fun isFocused(): Boolean {
+        return true
+    }
+}
---
+++ app/src/main/java/com/example/guys/ToneProcessView.kt
@@ -0,0 +1,111 @@
+package com.example.guys
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by liujiannan on 2020-05-17
+ */
+
+class ToneProcessView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val MIN_TONE = 1
+        private const val DEFAULT_MAX_TONE = 11
+        private const val DEFAULT_STROKE_WIDTH_DIP = 2.73f
+        private const val PER_HEIGHT_DIP = 1.83f
+        private const val MIN_HEIGHT_DIP = 2.74f
+        private const val DEFAULT_RADIUS_DIP = 3f
+        private const val DEFAULT_NORMAL_COLOR = 0x29FFFFFF
+        private const val DEFAULT_HIGHLIGHT_COLOR = 0xFFC421
+    }
+
+    private val highLightPaint = Paint()
+    private val normalPaint = Paint()
+    private val strokeWidth: Int
+    private var interval: Int
+    private val maxTone: Int
+    private val radius: Int
+    private var currentTone: Int = 3
+    private val rectF: RectF = RectF()
+
+    init {
+        val a = context.obtainStyledAttributes(attrs, R.styleable.ToneProcessView)
+        maxTone = a.getInteger(R.styleable.ToneProcessView_max_tone, DEFAULT_MAX_TONE)
+        if (maxTone < MIN_TONE) {
+            throw java.lang.IllegalStateException("max tone must be more than min tone($MIN_TONE)")
+        }
+        radius = a.getDimensionPixelSize(R.styleable.ToneProcessView_radius, dip2Px(DEFAULT_RADIUS_DIP).toInt())
+        strokeWidth = a.getDimensionPixelSize(R.styleable.ToneProcessView_stroke_width, dip2Px(DEFAULT_STROKE_WIDTH_DIP).toInt())
+        interval = a.getDimensionPixelSize(R.styleable.ToneProcessView_interval, -1)
+        highLightPaint.color = a.getColor(R.styleable.ToneProcessView_highlight_color, DEFAULT_HIGHLIGHT_COLOR)
+        highLightPaint.isDither = true
+        highLightPaint.isAntiAlias = true
+        highLightPaint.style = Paint.Style.FILL_AND_STROKE
+        normalPaint.color = a.getColor(R.styleable.ToneProcessView_normal_color, DEFAULT_NORMAL_COLOR)
+        normalPaint.isDither = true
+        normalPaint.isAntiAlias = true
+        normalPaint.style = Paint.Style.FILL_AND_STROKE
+
+        a.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        if (interval == -1) {
+            interval = (width - maxTone * strokeWidth) / (maxTone - 1)
+        }
+
+        for (i in MIN_TONE..maxTone) {
+            val radius = dip2Px(DEFAULT_RADIUS_DIP)
+            val startX = (i - 1) * (strokeWidth + interval).toFloat()
+            rectF.left = startX
+            rectF.right = startX + strokeWidth
+            rectF.top = height - dip2Px(MIN_HEIGHT_DIP) - (i - 1) * dip2Px(PER_HEIGHT_DIP)
+            rectF.bottom = height.toFloat()
+            if (i <= currentTone) {
+                canvas?.drawRoundRect(rectF,
+                        radius, radius, highLightPaint)
+            } else {
+                canvas?.drawRoundRect(rectF,
+                        radius, radius, normalPaint)
+            }
+        }
+    }
+
+    public fun increaseTone() {
+        currentTone++
+        invalidate()
+    }
+
+    public fun decreaseTone() {
+        currentTone--
+        invalidate()
+    }
+
+    public fun setTone(value: Int) {
+        currentTone = value
+        invalidate()
+    }
+
+    private fun checkState() {
+        if (currentTone < MIN_TONE || currentTone > maxTone) {
+            throw IllegalStateException("tone must be a value in [$MIN_TONE, $maxTone] ")
+        }
+    }
+
+    public fun getCurrentTone(): Int {
+        return currentTone
+    }
+
+    private fun dip2Px(dipValue: Float): Float {
+        val scale = context.resources.displayMetrics.density
+        return dipValue * scale + 0.5f
+    }
+}
